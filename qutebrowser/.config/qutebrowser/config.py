964938b4c2e0546383797f6c4ef70ef1daf6dc64
pointing <pointing@disroot.org>
2026-08-22 23:40:36 +0900

    nix home files -> dotfiles

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -46,29 +46,6 @@ config.load_autoconfig(False)
 #   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
 #   - never: Don't accept cookies at all.
 config.set("content.cookies.accept", "no-3rdparty", "chrome-devtools://*")
-
-# Which cookies to accept. With QtWebEngine, this setting also controls
-# other features with tracking capabilities similar to those of cookies;
-# including IndexedDB, DOM storage, filesystem API, service workers, and
-# AppCache. Note that with QtWebKit, only `all` and `never` are
-# supported as per-domain values. Setting `no-3rdparty` or `no-
-# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
-# `all`. If this setting is used with URL patterns, the pattern gets
-# applied to the origin/first party URL of the page making the request,
-# not the request URL. With QtWebEngine 5.15.0+, paths will be stripped
-# from URLs, so URL patterns using paths will not match. With
-# QtWebEngine 5.15.2+, subdomains are additionally stripped as well, so
-# you will typically need to set this setting for `example.com` when the
-# cookie is set on `somesubdomain.example.com` for it to work properly.
-# To debug issues with this setting, start qutebrowser with `--debug
-# --logfilter network --debug-flag log-cookies` which will show all
-# cookies being set.
-# Type: String
-# Valid values:
-#   - all: Accept all cookies.
-#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
-#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
-#   - never: Don't accept cookies at all.
 config.set("content.cookies.accept", "no-3rdparty", "devtools://*")
 
 # Value to send in the `Accept-Language` header. Note that the value
@@ -96,20 +73,6 @@ config.set(
     "https://web.whatsapp.com/",
 )
 
-# User agent to send.  The following placeholders are defined:  *
-# `{os_info}`: Something like "X11; Linux x86_64". * `{webkit_version}`:
-# The underlying WebKit version (set to a fixed value   with
-# QtWebEngine). * `{qt_key}`: "Qt" for QtWebKit, "QtWebEngine" for
-# QtWebEngine. * `{qt_version}`: The underlying Qt version. *
-# `{upstream_browser_key}`: "Version" for QtWebKit, "Chrome" for
-# QtWebEngine. * `{upstream_browser_version}`: The corresponding
-# Safari/Chrome version. * `{qutebrowser_version}`: The currently
-# running qutebrowser version.  The default value is equal to the
-# unchanged user agent of QtWebKit/QtWebEngine.  Note that the value
-# read from JavaScript is always the global value. With QtWebEngine
-# between 5.12 and 5.14 (inclusive), changing the value exposed to
-# JavaScript requires a restart.
-# Type: FormatString
 config.set(
     "content.headers.user_agent",
     "Mozilla/5.0 ({os_info}; rv:133.0) Gecko/20100101 Firefox/133.0",
@@ -119,25 +82,13 @@ config.set(
 # Load images automatically in web pages.
 # Type: Bool
 config.set("content.images", True, "chrome-devtools://*")
-
-# Load images automatically in web pages.
-# Type: Bool
 config.set("content.images", True, "devtools://*")
 
 # Enable JavaScript.
 # Type: Bool
 config.set("content.javascript.enabled", True, "chrome-devtools://*")
-
-# Enable JavaScript.
-# Type: Bool
 config.set("content.javascript.enabled", True, "devtools://*")
-
-# Enable JavaScript.
-# Type: Bool
 config.set("content.javascript.enabled", True, "chrome://*/*")
-
-# Enable JavaScript.
-# Type: Bool
 config.set("content.javascript.enabled", True, "qute://*/*")
 
 # Allow locally loaded documents to access remote URLs.
@@ -181,8 +132,8 @@ c.content.geolocation = False
 
 # c.zoom.default = '150%'
 
-# config.bind('<Ctrl-=>', 'zoom-in')
-# config.bind('<Ctrl-->', 'zoom-out')
+config.bind('<Ctrl-=>', 'zoom-in')
+config.bind('<Ctrl-->', 'zoom-out')
 
 # c.url.start_pages = ["about:blank"]
 # c.url.default_page = "about:blank"
@@ -191,7 +142,6 @@ c.content.geolocation = False
 
 ## Next works on Windows
 c.downloads.location.directory = "~/Downloads/"
-
 c.downloads.location.suggestion = "both"
 
 ## “yank asciidoc-formatted link”
@@ -209,7 +159,6 @@ c.content.dns_prefetch = False
 # c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/html?q={}'}
 # c.url.searchengines = {'DEFAULT': 'https://lite.qwant.com/?q={}'}
 
-# my personal config
 c.auto_save.session = True
 c.session.lazy_restore = True
 c.content.webgl = False
